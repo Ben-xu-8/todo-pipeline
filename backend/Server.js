a9e7a14e517c4903764320b29474fb15d593c112
Ben <xuben1996@gmail.com>
2023-10-13 00:37:42 -0400

    changed routes

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes/ToDoRoute');
 
@@ -9,6 +10,22 @@ require('dotenv').config();
 const app = express();
 
 const PORT = process.env.port || 5000;
+
+const _dirname = path._dirname('');
+const buildPath = path.join(_dirname, '../frontend/build');
+
+app.use(express.static(buildPath));
+
+app.get('/', function (req, res) {
+  res.sendFile(
+    path.join(__dirname, '../frontend/build/index.html'),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 app.use(express.json());
 app.use(cors());
